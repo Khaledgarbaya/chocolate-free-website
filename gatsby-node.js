c2f61d96d2864a515e59ac23b139775d6f650619
Khaled Garbaya <khaledgarbaya@gmail.com>
2019-10-27 00:53:45 +0200

    Migrate home page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,87 +1,79 @@
-const _ = require(`lodash`)
-const Promise = require(`bluebird`)
-const path = require(`path`)
-const slash = require(`slash`)
-const paginate = require('./utils/paginate')
+const _ = require(`lodash`);
+const Promise = require(`bluebird`);
+const path = require(`path`);
+const slash = require(`slash`);
+const paginate = require('./utils/paginate');
 
-// Implement the Gatsby API “createPages”. This is
-// called after the Gatsby bootstrap is finished so you have
-// access to any information necessary to programatically
-// create pages.
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
   return new Promise((resolve, reject) => {
-    // The “graphql” function allows us to run arbitrary
-    // queries against the local Contentful graphql schema. Think of
-    // it like the site has a built-in database constructed
-    // from the fetched data that you can run queries against.
     graphql(
       `
-      {
-        allContentfulArticle(limit: 1000) {
-          edges {
-            node {
-              id
-              slug
+        {
+          allContentfulArticle(limit: 1000) {
+            edges {
+              node {
+                id
+                slug
+              }
+            }
+          }
+          allContentfulPage {
+            edges {
+              node {
+                slug
+              }
             }
           }
         }
-      }
-    `
+      `
     )
       .then(result => {
         if (result.errors) {
-          reject(result.errors)
+          reject(result.errors);
         }
-        const articleTemplate = path.resolve(`./src/templates/article.js`)
-        const pageTemplate = path.resolve('./src/templates/Home/index.js')
-        paginate(
-          createPage,
-          pageTemplate,
-          '/article',
-          result.data.allContentfulArticle.edges.length,
-          6
-        )
-        console.log('after paginate')
-        _.each(result.data.allContentfulArticle.edges, edge => {
-          // Gatsby uses Redux to manage its internal state.
-          // Plugins and sites can use functions like "createPage"
-          // to interact with Gatsby.
+        const articleTemplate = path.resolve(`./src/templates/article.js`);
+        const pageTemplate = path.resolve('./src/templates/page.js');
+
+        _.each(result.data.allContentfulPage.edges, edge => {
           createPage({
-            // Each page is required to have a `path` as well
-            // as a template component. The `context` is
-            // optional but is often necessary so the template
-            // can query data specific to each page.
+            path: edge.node.slug,
+            component: slash(pageTemplate),
+            context: {
+              slug: edge.node.slug,
+            },
+          });
+        });
+        _.each(result.data.allContentfulArticle.edges, edge => {
+          createPage({
             path: `/article/${edge.node.slug}.html`,
             component: slash(articleTemplate),
             context: {
               id: edge.node.id,
-              slug: edge.node.slug
+              slug: edge.node.slug,
             },
-          })
-        })
+          });
+        });
       })
-      .then(resolve)
-  })
-}
-// Implement the Gatsby API “onCreatePage”. This is
-// called after every page is created.
+      .then(resolve);
+  });
+};
 exports.onCreatePage = async ({ page, actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
   return new Promise((resolve, reject) => {
     if (page.path.match(/^\/HorsSujet/)) {
-      page.matchPath = "/hors-sujet.html"
+      page.matchPath = '/hors-sujet.html';
 
       // Update the page.
-      createPage(page)
+      createPage(page);
     }
 
     if (page.path.match(/^\/About/)) {
-      page.matchPath = "/about.html"
+      page.matchPath = '/about.html';
 
-      createPage(page)
+      createPage(page);
     }
 
-    resolve()
-  })
-}
+    resolve();
+  });
+};
