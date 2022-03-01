d2b97808276979e2eccef214c896693f7f04fb4a
Khaled Garbaya <kgarbaya@gmail.com>
2022-03-01 14:08:56 +0100

    Prettify the print button

--- src/components/content-modules/article-recipe.js
+++ src/components/content-modules/article-recipe.js
@@ -51,12 +51,28 @@ class Recipe extends Component {
             <div className="w-full md:w-1/2 print:w-1/2">
               <GatsbyImage alt={title} image={getImage(image)} />
               <button
-                className="print-button print:hidden"
+                className="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 mr-2 mt-2 print:hidden"
                 onClick={() => {
                   this.print();
                 }}
               >
-                Print <span className="print-icon"></span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="mr-2 -ml-1 w-5 h-5"
+                  aria-hidden="true"
+                  focusable="false"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"
+                  />
+                </svg>
+                <span>Print</span>
               </button>
             </div>
           </div>
