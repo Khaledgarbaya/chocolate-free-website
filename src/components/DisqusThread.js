7d2ef4048ab3a2b454a0ff4c3a1f5875086048eb
Khaled Garbaya <kgarbaya@gmail.com>
2022-10-31 14:27:16 +0100

    refactor: embed Disqus using the Script comp

--- src/components/DisqusThread.js
+++ src/components/DisqusThread.js
@@ -1,55 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Script } from "gatsby";
 
-const SHORTNAME = 'chocolate-free';
-const WEBSITE_URL = 'https://chocolate-free.com';
-
-function renderDisqus() {
-  if (window.DISQUS === undefined) {
-    var script = document.createElement('script')
-    script.async = true
-    script.src = 'https://' + SHORTNAME + '.disqus.com/embed.js'
-    document.getElementsByTagName('head')[0].appendChild(script)
-  } else {
-    window.DISQUS.reset({reload: true})
-  }
-}
-
-class DisqusThread extends React.Component{
-
-  static propTypes = {
-    id: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    path: PropTypes.string.isRequired
-  };
-
-  shouldComponentUpdate(nextProps) {
-    return this.props.id !== nextProps.id ||
-      this.props.title !== nextProps.title ||
-      this.props.path !== nextProps.path;
-  }
-
-  componentDidMount() {
-    renderDisqus()
-  }
-
-  componentDidUpdate() {
-    renderDisqus()
-  }
-
-  render() {
-    let { id, title, path, ...other} = this.props
-
-    if (process.env.BROWSER) {
-      window.disqus_shortname = SHORTNAME
-      window.disqus_identifier = id
-      window.disqus_title = title
-      window.disqus_url = WEBSITE_URL + path
-    }
-
-    return <div {...other} id="disqus_thread" className='print:hidden'/>
-  }
-
-}
-
-export default DisqusThread
+const SHORTNAME = "chocolate-free";
+const WEBSITE_URL = "https://chocolate-free.com";
+const DisqusThread = ({ id, title, path }) => {
+  return (
+    <>
+      <Script
+        src={`https://${SHORTNAME}.disqus.com/embed.js`}
+        onLoad={() => {
+          window.disqus_shortname = SHORTNAME;
+          window.disqus_identifier = id;
+          window.disqus_title = title;
+          window.disqus_url = WEBSITE_URL + path;
+        }}
+      />
+      <div id="disqus_thread"></div>
+    </>
+  );
+};
+export default DisqusThread;
